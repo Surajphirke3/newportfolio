21135a98c6a0ba3f5a81b6ce3cee180e3b69f737
Surajphirke3 <surajphirke3@gmail.com>
2025-05-05 12:22:15 +0530

    added projects

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -174,18 +174,7 @@ const about = {
                     
         // optional: leave the array empty if you don't want to display images
         images: [
-          {
-            src: "/images/projects/project-01/cover-02.jpg",
-            alt: "Project image",
-            width: 16,
-            height: 9,
-          },
-          {
-            src: "/images/projects/project-01/cover-03.jpg",
-            alt: "Project image",
-            width: 16,
-            height: 9,
-          },
+          
         ],
       },
       {
@@ -214,12 +203,7 @@ const about = {
       </>,
         // optional: leave the array empty if you don't want to display images
         images: [
-          {
-            src: "/images/projects/project-01/cover-04.jpg",
-            alt: "Project image",
-            width: 16,
-            height: 9,
-          },
+          
         ],
       },
     ],
